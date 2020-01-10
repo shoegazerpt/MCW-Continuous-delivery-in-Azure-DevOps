e90a883e9ed7c2b29cbb984ca7b0905ccc691438
Ricardo Fiel <rifiel@microsoft.com>
2020-01-10 14:09:18 +0000

    Fix template link

--- Hands-on lab/deploy/deploy.sh
+++ Hands-on lab/deploy/deploy.sh
@@ -4,7 +4,7 @@ curl -o studentfiles.zip https://cloudworkshop.blob.core.windows.net/agile-conti
 unzip studentfiles.zip
 
 rgName=${1:-$(az group list --query [0].name -o tsv)}
-templateUri=https://raw.githubusercontent.com/shoegazerpt/MCW-Serverless-architecture/btf2020/Hands-on%20lab/deploy/azureDeploy.json
+templateUri=https://raw.githubusercontent.com/shoegazerpt/MCW-Continuous-delivery-in-Azure-DevOps/btf2020/Hands-on%20lab/deploy/azureDeploy.json
 
 echo ""
 echo "Provisioning test and production environments..."
