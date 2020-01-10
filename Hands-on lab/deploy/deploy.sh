423f961505c7d80d598aba5584fdb7866f5eb2f5
Ricardo Fiel <rifiel@microsoft.com>
2020-01-10 13:55:52 +0000

    Added default values to arm

--- Hands-on lab/deploy/deploy.sh
+++ Hands-on lab/deploy/deploy.sh
@@ -2,3 +2,9 @@
 
 curl -o studentfiles.zip https://cloudworkshop.blob.core.windows.net/agile-continous-delivery/studentfiles.zip
 unzip studentfiles.zip
+
+rgName=${1:-$(az group list --query [0].name -o tsv)}
+rgLocation=${2:-$(az group list --query [0].location -o tsv)}
+templateFile=https://raw.githubusercontent.com/shoegazerpt/MCW-Serverless-architecture/btf2020/Hands-on%20lab/deploy/azureDeploy.json
+
+az group deployment create --resource-group $resourceGroupName --template-file "$HOME/studentfiles/armtemplate/azuredeploy.json"
